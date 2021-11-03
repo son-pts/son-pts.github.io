135fda25ab00b50214fdc0246144a981fbf14c37
Sơn Sói Xám <91018946+son-pts@users.noreply.github.com>
2021-11-03 22:46:45 +0700

    xog 01

--- assets/js/main.js
+++ assets/js/main.js
@@ -55,25 +55,25 @@ sr.reveal('.home__scroll', {origin:'bottom', delay: 200})
 sr.reveal('.home__img', {origin:'right', delay: 400})
 
 /*SCROLL ABOUT*/
-sr.reveal('.about__img', {delay: 500})
-sr.reveal('.about__subtitle', {delay: 300})
-sr.reveal('.about__profession', {delay: 400})
-sr.reveal('.about__text', {delay: 500})
-sr.reveal('.about__social-icon', {delay: 600, interval: 200})
+sr.reveal('.about__img', {delay: 400})
+sr.reveal('.about__subtitle', {delay: 200})
+sr.reveal('.about__profession', {delay: 300})
+sr.reveal('.about__text', {delay: 400})
+sr.reveal('.about__social-icon', {delay: 500, interval: 200})
 
 /*SCROLL SKILLS*/
 sr.reveal('.skills__subtitle', {})
-sr.reveal('.skills__name', {distance: '100px', delay: 50, interval: 100})
-sr.reveal('.skills__img', {delay: 400})
+sr.reveal('.skills__name', {distance: '100px', delay: 30, interval: 100})
+sr.reveal('.skills__name-buttom', {origin:'bottom',distance: '100px', delay: 30, interval: 100})
+sr.reveal('.skills__img', {delay: 300})
 
 /*SCROLL PORTFOLIO*/
 sr.reveal('.portfolio__img', {interval: 200})
+sr.reveal('.portfolio__img-buttom', {origin:'bottom',interval: 200})
 
 /*SCROLL CONTACT*/
-sr.reveal('.contact__subtitle', {})
-sr.reveal('.contact__text', {interval: 200})
-sr.reveal('.contact__input', {delay: 400})
-sr.reveal('.contact__button', {delay: 600})
+sr.reveal('.contact__subtitle', {delay: 30})
+sr.reveal('.contact__text', {delay: 30,interval: 200})
 
 //audio 
 sr.reveal('.audio', {origin:'bottom', delay: 400})
